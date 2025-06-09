c51c65872cd3af492d8ee6efd0b826d5875cd0d1
Carlos Martinez <40340506+antichals@users.noreply.github.com>
2025-06-09 13:43:16 +0200

    Polished reticle

--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -7,17 +7,21 @@ public class Reticle : MonoBehaviour
 {
     private RectTransform reticle;
     [SerializeField, Range(50, 250f)]
-    private float size;
+    private float currentSize;
+
+    // TODO change on isMoving state
+    private float maxSize;
+    private float restingSize;
 
     private void Start()
     {
         reticle = GetComponent<RectTransform>();
-        size = 100;
+        currentSize = 100;
 
     }
 
     private void Update()
     {
-        reticle.sizeDelta = new Vector2(size, size);
+        reticle.sizeDelta = new Vector2(currentSize, currentSize);
     }
 }
